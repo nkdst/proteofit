2e1cc088714d5493da45772b63b45f81f1364f0f
Nick Diercksen <nick@diercksen.eu>
2022-11-15 14:01:38 +0100

    experiments

---
+++ code/analysis/00 remoteRender.R
@@ -0,0 +1,35 @@
+project.wd <- rprojroot::find_root(".Rhistory")
+
+# parameters
+pCutoff <- 0.01
+FCutoff <- 1
+
+
+# path.analysis <- file.path("./code/analysis");
+path.analysis <- file.path(".", "code", "analysis");
+
+
+# 03
+
+rmd.params <- list(pCutoff = pCutoff, FCutoff = FCutoff)
+filename.addition <- ""
+rmarkdown::render(
+  file.path(path.analysis, "03_DESeq.Rmd"),
+  output_file = file.path(path.analysis, paste0("03_DESeq", filename.addition, ".html")),
+  output_dir = path.analysis,
+  output_format = "html_document",
+  params = rmd.params,
+  knit_root_dir = project.wd
+)
+
+
+# testing
+
+rmarkdown::render(
+  file.path("./code/testing.Rmd"),
+  output_file = "./code/testing.html",
+  output_dir = "./code",
+  output_format = "html_document",
+  # output_format = rmarkdown::html_notebook(toc = TRUE, toc_float = TRUE),
+  knit_root_dir = project.wd
+)
