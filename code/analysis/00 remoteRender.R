6566354a71d03e32e321177f4c783b938a338f7d
Nick Diercksen <nick@diercksen.eu>
2022-11-16 15:24:11 +0100

    04 used plotly and added param fgsea.maxSize to rmd

--- code/analysis/00 remoteRender.R
+++ code/analysis/00 remoteRender.R
@@ -22,6 +22,18 @@ rmarkdown::render(
   knit_root_dir = project.wd
 )
 
+# 04
+
+rmd.params <- list(pCutoff = pCutoff, reevaluate = T)
+filename.addition <- "_maxSize200_"
+rmarkdown::render(
+  file.path(path.analysis, "04_fgsea.Rmd"),
+  output_file = file.path(path.analysis, paste0("04_fgsea", filename.addition, ".html")),
+  output_dir = path.analysis,
+  output_format = "html_document",
+  params = rmd.params,
+  knit_root_dir = project.wd
+)
 
 # testing
 
