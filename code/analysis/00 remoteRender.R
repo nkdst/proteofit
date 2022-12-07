b53d3f8042e1ff1cc8319d3fed8ad8cf8278479b
Nick Diercksen <nick@diercksen.eu>
2022-12-07 23:29:57 +0100

    reordered files, created dedicated reports folder

--- code/analysis/00 remoteRender.R
+++ code/analysis/00 remoteRender.R
@@ -5,8 +5,9 @@ pCutoff <- 0.01
 FCutoff <- 1
 
 
-# path.analysis <- file.path("./code/analysis");
-path.analysis <- file.path(".", "code", "analysis");
+# path.analysis <- file.path("./code/analysis")
+path.analysis <- file.path(".", "code", "analysis")
+path.reports <- file.path(".", "reports")
 
 
 # 03
@@ -16,7 +17,7 @@ filename.addition <- ""
 rmarkdown::render(
   file.path(path.analysis, "03_DESeq.Rmd"),
   output_file = file.path(path.analysis, paste0("03_DESeq", filename.addition, ".html")),
-  output_dir = path.analysis,
+  output_dir = path.reports,
   output_format = "html_document",
   params = rmd.params,
   knit_root_dir = project.wd
@@ -29,11 +30,25 @@ filename.addition <- "_maxSize400_"
 rmarkdown::render(
   file.path(path.analysis, "04_fgsea.Rmd"),
   output_file = file.path(path.analysis, paste0("04_fgsea", filename.addition, ".html")),
-  output_dir = path.analysis,
+  output_dir = path.reports,
   output_format = "html_document",
   params = rmd.params,
   knit_root_dir = project.wd
 )
+
+# 05
+
+rmd.params <- list(reevaluate = F)
+filename.addition <- ""
+rmarkdown::render(
+  file.path(path.analysis, "05_fgseaRes_pathways.Rmd"),
+  output_file = file.path(path.analysis, paste0("05_fgseaRes_pathways", filename.addition, ".html")),
+  output_dir = path.reports,
+  output_format = "html_document",
+  params = rmd.params,
+  knit_root_dir = project.wd
+)
+
 
 # testing
 
