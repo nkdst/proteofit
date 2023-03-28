574a37a5fac0bfdd91b863ac62763734d8e546ec
Nick Diercksen <nick@diercksen.eu>
2023-03-28 16:04:09 +0200

    refactored code and added color knitting parameters

--- code/analysis/00 remoteRender.R
+++ code/analysis/00 remoteRender.R
@@ -1,78 +1,103 @@
+# libraries ----
+library(dplyr)
+
+
+
+# parameters ----
+
+## path variabls ----
 project.wd <- rprojroot::find_root(".Rhistory")
-
-# parameters
-pCutoff <- 0.01
-FCutoff <- 1
-
-
-# path.analysis <- file.path("./code/analysis")
 path.analysis <- file.path(".", "code", "analysis")
 path.reports <- file.path(".", "reports")
 
+## document parameters ----
+pCutoff <- 0.01
+FCutoff <- 1
+condition_pal <- c("WT" = "#00C3C6", "KO" = "#FF6C67")
+tissue_pal <- c(gastroc = "orange", soleus = "purple")
+# tissue_pal <- c(gastroc = "#FFB08E", soleus="#FF6638")
 
-# 03
+# parameter vector containing all general parameters:
+all_parameters <-
+  list(
+    pCutoff = pCutoff,
+    FCutoff = FCutoff,
+    tissue_pal = tissue_pal,
+    condition_pal = condition_pal
+  )
 
-rmd.params <- list(pCutoff = pCutoff, FCutoff = FCutoff)
-filename.addition <- ""
-rmarkdown::render(
-  file.path(path.analysis, "03_DESeq.Rmd"),
-  output_file = file.path(path.analysis, paste0("03_DESeq", filename.addition, ".html")),
-  output_dir = path.reports,
-  output_format = "html_document",
-  params = rmd.params,
-  knit_root_dir = project.wd
+
+# render functions ----
+
+
+# ' this function looks at the yaml of the target file and extracts then only those
+# ' given parameters which are needed for this document.
+getFittingParametersForDocument <- function(filepath, parameters) {
+  document.params <- rmarkdown::yaml_front_matter(filepath)$params
+  
+  needed_params <- names(parameters) %in% names(document.params) %>%
+    parameters[.]
+  return(needed_params)
+}
+
+# ' rendering the given Rmd file to a `html_document`
+customMarkdownRendering <- function(filename,
+                                    filename.addition = "",
+                                    parameters = all_parameters,
+                                    path.input = path.analysis,
+                                    path.output = path.reports,
+                                    path.root = project.wd) {
+  filepath <-
+    file.path(path.input, filename) # no file-extension yet
+  filepath.rmd <- paste0(filepath, ".Rmd")
+  rmd.params <-
+    getFittingParametersForDocument(filepath = filepath.rmd, parameters)
+  
+  
+  rmarkdown::render(
+    input = paste0(filepath, ".Rmd"),
+    output_file = paste0(filepath, filename.addition, ".html"),
+    output_dir = path.output,
+    output_format = "html_document",
+    params = rmd.params,
+    knit_root_dir = path.root
+  )
+}
+
+
+# Documents ----
+
+## 03 DESeq ----
+customMarkdownRendering(filename = "03_DESeq")
+
+
+## 04 fgsea ----
+customMarkdownRendering(
+  filename = "04_fgsea",
+  filename.addition = "_maxSize200_",
+  params = c(parameters, reevaluate = F, fgsea.maxSize = 200)
 )
 
-# 04
 
-rmd.params <- list(pCutoff = pCutoff, reevaluate = F, fgsea.maxSize = 200)
-filename.addition <- "_maxSize400_"
-rmarkdown::render(
-  file.path(path.analysis, "04_fgsea.Rmd"),
-  output_file = file.path(path.analysis, paste0("04_fgsea", filename.addition, ".html")),
-  output_dir = path.reports,
-  output_format = "html_document",
-  params = rmd.params,
-  knit_root_dir = project.wd
-)
-
-# 05
-
-rmd.params <- list(reevaluate = F)
-filename.addition <- ""
-rmarkdown::render(
-  file.path(path.analysis, "05_fgseaRes_pathways.Rmd"),
-  output_file = file.path(path.analysis, paste0("05_fgseaRes_pathways", filename.addition, ".html")),
-  output_dir = path.reports,
-  output_format = "html_document",
-  params = rmd.params,
-  knit_root_dir = project.wd
-)
+## 05 fgseaRes pathways ----
+customMarkdownRendering(filename = "05_fgseaRes_pathways",
+                        parameters = c(all_parameters, reevaluate = FALSE))
 
 
-
-# 07
-
-rmd.params <- list(pCutoff = pCutoff, FCutoff = FCutoff)
-filename.addition <- ""
-rmarkdown::render(
-  file.path(path.analysis, "07_currentPlots.Rmd"),
-  output_file = file.path(path.analysis, paste0("07_currentPlots", filename.addition, ".html")),
-  output_dir = path.reports,
-  output_format = "html_document",
-  params = rmd.params,
-  knit_root_dir = project.wd
-)
+## 06 Proteomics ----
+customMarkdownRendering(filename = "06.1_Proteomics")
 
 
+## 07 currentPlots ----
+customMarkdownRendering(filename = "07_currentPlots")
 
-# testing
 
-rmarkdown::render(
-  file.path("./code/testing.Rmd"),
-  output_file = "./code/testing.html",
-  output_dir = "./code",
-  output_format = "html_document",
-  # output_format = rmarkdown::html_notebook(toc = TRUE, toc_float = TRUE),
-  knit_root_dir = project.wd
-)
+## testing ----
+# rmarkdown::render(
+#   file.path("./code/testing.Rmd"),
+#   output_file = "./code/testing.html",
+#   output_dir = "./code",
+#   output_format = "html_document",
+#   # output_format = rmarkdown::html_notebook(toc = TRUE, toc_float = TRUE),
+#   knit_root_dir = project.wd
+# )
